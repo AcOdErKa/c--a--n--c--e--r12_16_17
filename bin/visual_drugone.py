f9ce4df4b35a4703558438cc9f5865e7ad3a96ad
AcOdErKa <akar.bhisek@gmail.com>
2018-02-19 13:51:25 +0530

    Add files via upload

--- bin/visual_drugone.py
+++ bin/visual_drugone.py
@@ -8,13 +8,61 @@ Created on Wed Feb  7 13:51:38 2018
 
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.style.use("bmh")
 
-df_drugone=pd.read_csv("output_drugone.csv",index_col=0)
+df_drugone=pd.read_csv("outs/output_drugone.csv",index_col=0)
+#
+#plt.figure()
+#plt.title("DRUG VECTORS V FAULT LOCATIONS")
+#plt.imshow(df_drugone.iloc[:,1:],interpolation="sinc",cmap=plt.cm.RdYlGn_r)
+#plt.axis("tight")
+#plt.xticks([i for i in range(25)],df_drugone.columns[1:])
+#plt.xlabel("Fault Location")
+#plt.yticks([i for i in range(64)],df_drugone.iloc[:,0])
+#plt.ylabel("Drug Vector")
+#plt.colorbar()
+#plt.show()
 
+df_con=pd.DataFrame(columns=["Drug Vector","Encoded Weightage"])
+df_con.iloc[:,0]=df_drugone.iloc[:,0]
+
+for i in range(len(df_drugone.index)):
+    weight=0
+    for j in range(25):
+        weight=weight+df_drugone.iloc[i,j+1]
+    df_con.iloc[i,1]=weight
+  
+df_con.iloc[:,1]=df_con.iloc[:,1]/(df_con.iloc[:,1].max()/2)
+df_con.to_csv("outs/output_drugone_weight.csv")
+df_con=pd.read_csv("outs/output_drugone_weight.csv",index_col=0)
+   
+#plt.figure()
+#plt.title("DRUG VECTORS V ENCODED WEIGHTS")
+#plt.imshow(df_con.iloc[:,1:],interpolation="nearest",cmap=plt.cm.RdYlGn_r)
+#plt.axis("tight")
+#plt.xticks([],[])
+#plt.xlabel("Encoded Weight")
+#plt.yticks([i for i in range(64)],df_con.iloc[:,0])
+#plt.ylabel("Drug Vector")
+#plt.colorbar()
+#plt.show()
+
+df_same=pd.DataFrame(columns=df_con.columns)
+
+j=0
+for i in range(64):
+    if df_con.iloc[i,1].item()==df_con.iloc[:,1].min():
+        df_same.loc[j]=df_con.loc[i]
+        j=j+1
+
+#print(df_same)
 plt.figure()
-plt.imshow(df_drugone.iloc[:,1:],interpolation=None,cmap=plt.cm.jet)
-
-plt.xticks([i for i in range(25)],df_drugone.columns[1:])
-plt.yticks([i for i in range(64)],df_drugone.iloc[:,0])
-plt.colorbar()
+plt.title("DRUG VECTORS V ENCODED WEIGHTS")
+plt.scatter(df_con.iloc[:,0],df_con.iloc[:,1])#df_same["Encoded Weightage"],df_same["Drug Vector"])
+plt.axis("tight")
+#plt.xticks([],[])
+plt.xlabel("Drug Vector")
+plt.xticks([i for i in range(64)],df_con.iloc[:,0],rotation="vertical")
+plt.ylabel("Encoded Weight")
+plt.legend()
 plt.show()
--- bin/visual_drugtwo.py
+++ bin/visual_drugtwo.py
@@ -7,12 +7,39 @@ Created on Wed Feb  7 14:44:50 2018
 """
 import pandas as pd
 import matplotlib.pyplot as plt
+plt.style.use("bmh")
 
-df_drugtwo=pd.read_csv("output_drugtwo.csv",index_col=0)
+ifile="outs/output_drugtwo2.csv"
+df_drugtwo=pd.read_csv(ifile,index_col=0)
 
 plt.figure()
-plt.imshow(df_drugtwo.iloc[:,1:],interpolation=None,cmap=plt.cm.jet)
+plt.imshow(df_drugtwo.iloc[:,1:],interpolation="sinc",cmap=plt.cm.RdYlGn_r)
+plt.axis("tight")
+plt.title(ifile)
 plt.xticks([i for i in range(276)],df_drugtwo.columns[1:])
 plt.yticks([i for i in range(64)],df_drugtwo.iloc[:,0])
+plt.colorbar()
+plt.show()
+
+df_con=pd.DataFrame(columns=["Drug Vector","Encoded weightage"])
+df_con.iloc[:,0]=df_drugtwo.iloc[:,0]
+
+for i in range(len(df_drugtwo.index)):
+    weight=0
+    for j in range(276):
+        weight=weight+df_drugtwo.iloc[i,j+1]
+    df_con.iloc[i,1]=weight
+    
+df_con.iloc[:,1]=df_con.iloc[:,1]/(df_con.iloc[:,1].max()/2)
+ofile="outs/output_drugtwo_weight2.csv"
+df_con.to_csv(ofile)
+df_con=pd.read_csv(ofile,index_col=0)
+   
+plt.figure()
+plt.imshow(df_con.iloc[:,1:],interpolation="nearest",cmap=plt.cm.RdYlGn_r)
+plt.axis("tight")
+plt.title(ofile)
+plt.xticks([],[])
+plt.yticks([i for i in range(64)],df_con.iloc[:,0])
 plt.colorbar()
 plt.show()
